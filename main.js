003d62aaa42e0887690f15f564a608cac26208f5
monish2011 <142167411+monish2011@users.noreply.github.com>
2024-02-24 11:44:42 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -10,15 +10,22 @@ var SpeechRecognition = window.webkitSpeechRecognition;
     var Content = event.results[0][0].transcript;
     console.log(Content);
     document.getElementById("textbox").innerHTML = Content;
-    speak();
+    if(Content="take my selfie"){
+      console.log("taking selfie ---");
+      speak();
+    }
  }
  function speak(){
     var synth = window.speechSynthesis;
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "taking your selfie in 5 seconds"
     var utterThis= new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
     Webcam.attach(camera);
+    setTimeout(function(){
+      take_snapshot();
+      save();
 
+    }, 5000 )
  }
  Webcam.set({
     width:360,
@@ -28,3 +35,16 @@ var SpeechRecognition = window.webkitSpeechRecognition;
 
  });
  camera = document.getElementById("camera");
+ function take_snapshot(){
+   Webcam.snap(function(data_uri){
+      document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+   });
+
+ }
+ function save(){
+   link= document.getElementById("link");
+   image = document.getElementById("selfie_image").src;
+   link.href=image;
+   link.click();
+
+ }
